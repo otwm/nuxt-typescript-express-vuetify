257dab3825bf2783d36454c71af2c779a786e20c
dong o kim <otwmoo@gmail.com>
2018-10-03 20:12:50 +0900

    # vuetify init

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,6 +56,9 @@ module.exports = {
   */
   css: ["~/assets/css/main.css"],
   build: {},
+  plugins: [
+    { src: '~/plugins/components' },
+  ],
   modules: [
     "@nuxtjs/axios",
     "~/modules/typescript.js"
